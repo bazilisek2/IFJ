9606389d79418797af4c6c5b828982d089b04a20
Fudgeee <57508483+Fudgeee@users.noreply.github.com>
2020-11-20 15:01:48 +0100

    Update symtable.c

--- symtable.c
+++ symtable.c
@@ -58,63 +58,6 @@ void TInsert (symtablePtr* RootPtr, char K, int Content)	{
 	}
 }
 
-void ReplaceByRightmost (symtablePtr PtrReplaced, symtablePtr*RootPtr) {
-/*   
-** Pomocná funkce pro vyhledání, přesun a uvolnění nejpravějšího uzlu.
-**/
-	if ((*RootPtr) != NULL) //hodnota ukazatela nebude NULL
-	{
-		if((*RootPtr)->RPtr != NULL) //ak neni najpravejsi uzol
-			ReplaceByRightmost(PtrReplaced, &(*RootPtr)->RPtr);
-		else
-		{
-			PtrReplaced->Data = (*RootPtr)->Data;
-			PtrReplaced->Key = (*RootPtr)->Key;
-		}
-	}
-}
-
-void TDelete (symtablePtr *RootPtr, char K) 		{
-/*   
-** Zruší uzel stromu, který obsahuje klíč K.
-**/
-	symtablePtr item = NULL;
-	if (((*RootPtr)->Key > K) && ((*RootPtr)->LPtr != NULL)) //ide dolava
-		TDelete(&(*RootPtr)->LPtr, K);
-	else if (((*RootPtr)->Key < K) && ((*RootPtr)->RPtr != NULL)) //ide doprava
-		TDelete(&(*RootPtr)->RPtr, K);
-	else if ((*RootPtr)->Key == K) //nachadzame sa na prvku ktory chceme odstranit
-	{
-		if (((*RootPtr)->RPtr == NULL) && ((*RootPtr)->LPtr == NULL)) //nema vpravo ani vlavo ziadny dalsi uzol
-		{
-		 	free(*RootPtr);
-			(*RootPtr) = NULL;
-		}
-		else if (((*RootPtr)->RPtr == NULL) && ((*RootPtr)->LPtr != NULL)) //ma dalsi uzol len vpravo
-
-		{
-			item = (*RootPtr);
-			(*RootPtr) = (*RootPtr)->LPtr;
-			free(item);
-			item = NULL;
-		}
-		else if (((*RootPtr)->RPtr != NULL) && ((*RootPtr)->LPtr == NULL)) //ma dalsi uzol len vlavo
-		{
-			item = (*RootPtr);
-			(*RootPtr) = (*RootPtr)->RPtr;
-			free(item);
-			item = NULL;
-		}
-		else
-		{
-			symtablePtr item1 = NULL;
-			item1 = (*RootPtr);
-			ReplaceByRightmost(item1, &(*RootPtr)->LPtr);
-			TDelete(&(*RootPtr)->LPtr, item1->Key);
-		}
-	}
-}
-
 void TDispose (symtablePtr *RootPtr) {
 // Zruší celý binární vyhledávací strom a korektně uvolní paměť.
 
