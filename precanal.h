a6a54bc60ee6d0b64f9df53152ed7a3125dd7cca
xbella01 <62183961+xbella01@users.noreply.github.com>
2020-12-09 19:36:22 +0100

    Update precanal.h

--- precanal.h
+++ precanal.h
@@ -1,3 +1,14 @@
+/****
+ * IFJ Projekt 2020
+ * 
+ * Autori:
+ * xbella01 - Magdaléna Bellayová 
+ * 
+ * Súhrn: Precedenčná analýza
+ * Prechádza tokeny vo výraze a uplatňuje na nich pravidlá  
+ * 
+ * */
+
 #ifndef PRECANAL_H
 #define PRECANAL_H
 #include "symtable.h"
